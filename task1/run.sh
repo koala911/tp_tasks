aad6dd4a0c6d771dd9dcad877bb5a1ef358e5c07
Константин <kovalev.ka@phystech.edu>
2020-02-24 01:30:52 +0300

    task1

--- task1/run.sh
+++ task1/run.sh
@@ -1,19 +1,19 @@
 dir_to_backup=$1
-mkdir $3 2> /dev/null
-ext=$2
+mkdir $3
+cpp=$2
 backup_dir=$(pwd)/$3
-cd $dir_to_backup 2> /dev/null
+cd $dir_to_backup
 dfs () {
   names=$(ls)
   for name in $names
   do
     if [ -d ./$name ] 
     then 
-      cd $name 2> /dev/null
+      cd $name
       dfs
-      cd .. 2> /dev/null
+      cd ..
     else
-      if [ ${name##*.} == $ext ] && [ -f ./$name ]
+      if [ ${name##*.} == $cpp ] && [ -f ./$name ]
       then
         new_file_name=$name
         k=1  
@@ -22,13 +22,13 @@ dfs () {
           new_file_name=$k$name
           ((k++))
         done
-        cp $name $backup_dir/$new_file_name 2> /dev/null
+        cp $name $backup_dir/$new_file_name
       fi
     fi
   done
 }
 dfs
-cd $backup_dir 2> /dev/null
-cd .. 2> /dev/null
+cd $backup_dir
+cd ..
 tar -cvf $4 $3 2> /dev/null
 echo "done"
