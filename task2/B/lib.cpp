5d183fef3aed3437ab89e9135092237a29abcf3f
Константин <kovalev.ka@phystech.edu>
2020-03-13 00:03:53 +0300

    .

---
+++ task2/B/lib.cpp
@@ -0,0 +1,5 @@
+#include "lib.h"
+
+int sqr(int a) {
+    return a * a;
+}
---
+++ task2/C/Test.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include "A/index.h"
+#include "B/lib.h"
+
+TEST(LibTest) {
+    EXPECT_EQ(25, sqr(5));
+    EXPECT_EQ(4, sqr(2));
+    EXPECT_EQ(100, sqr(10));
+    EXPECT_EQ(0, sqr(0));
+    EXPECT_EQ(4, sqr(-2));
+    EXPECT_EQ(100, sqr(-10));
+}
+
+TEST(IndexTest) {
+    EXPECT_EQ(1, x);
+}
