0388fab7bbe4095457798f73dc16cc18251245be
Константин <kovalev.ka@phystech.edu>
2020-03-31 00:37:30 +0300

    Добавил комментарии

--- TpPatterns/main.cpp
+++ TpPatterns/main.cpp
@@ -5,20 +5,31 @@
 int main() {
     Fraction* defending_fraction = new DefendingFraction;
     Fraction* attacking_fraction = new AttackingFraction;
-    defending_fraction->CreateNewSquad();
-    defending_fraction->AddBonusUnit(0);
-    defending_fraction->AddBonusUnit(0);
-    attacking_fraction->CreateNewSquad();
-    attacking_fraction->AddBonusUnit(0);
-    attacking_fraction->AddBonusUnit(0);
-    defending_fraction->Attack(*attacking_fraction, 0);
-    defending_fraction->Earn(0);
-    defending_fraction->Earn(0);
-    attacking_fraction->Earn(0);
-    attacking_fraction->Earn(0);
-    defending_fraction->BuyUnit<HorseRider>(0);
-    attacking_fraction->BuyUnit<Swordsman>(0);
+
+    defending_fraction->CreateNewSquad(); // создаем первый отряд в первой фракции
+
+    defending_fraction->AddBonusUnit(0); // добавлем двух
+    defending_fraction->AddBonusUnit(0); // бонусных юнитов (мечников)
+
+    attacking_fraction->CreateNewSquad(); // создаем первый отряд во второй фракции
+
+    attacking_fraction->AddBonusUnit(0); // добавлем двух
+    attacking_fraction->AddBonusUnit(0); // бонусных юнитов (лучников)
+
+    defending_fraction->Attack(*attacking_fraction, 0); // первая фракция атакует вторую
+
+    defending_fraction->Earn(0); // первая фракция
+    defending_fraction->Earn(0); // зарабатывает деньги
+
+    attacking_fraction->Earn(0); // вторая фракция
+    attacking_fraction->Earn(0); // зарабатывает деньги
+
+    defending_fraction->BuyUnit<HorseRider>(0); // первая фракция пытается купить юнита за 90,
+                                                             // имея 60
+    attacking_fraction->BuyUnit<Swordsman>(0); // вторая фракция пытается купить юнита за 50,
+                                                            // имея 60
     std::cout << "End if example" << std::endl;
+
     delete defending_fraction;
     delete attacking_fraction;
 }
