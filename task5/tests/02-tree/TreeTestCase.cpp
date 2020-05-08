d77be81ba1cf9d9c5cba0b90715b9afbc4670b62
Konstantin <kovalev.ka@phystech.edu>
2020-05-08 07:04:39 +0300

    Еще допилил тесты дерева

--- task5/tests/02-tree/TreeTestCase.cpp
+++ task5/tests/02-tree/TreeTestCase.cpp
@@ -14,10 +14,10 @@ TEST_F(TreeTestCase, TestOne) {
 }
 
 TEST_F(TreeTestCase, TestTwo) {
-    std::ofstream ofs("my_file_for_test.txt"); 
+    std::ofstream ofs("my_file_for_test.txt");
     ofs.close();
     ASSERT_THROW(GetTree("my_file_for_test.txt", false), std::invalid_argument);
-    std::remove("my_file_for_test.txt"); 
+    std::remove("my_file_for_test.txt");
 }
 
 TEST_F(TreeTestCase, TestThree) {
@@ -27,10 +27,10 @@ TEST_F(TreeTestCase, TestThree) {
 
 TEST_F(TreeTestCase, TestFour) {
     boost::filesystem::path dir("./my_dir_for_test");
-    boost::filesystem::create_directories(dir); 
+    boost::filesystem::create_directories(dir);
     boost::filesystem::path subdir("./my_dir_for_test/my_subdir");
-    boost::filesystem::create_directories(subdir); 
-    std::ofstream ofs("./my_dir_for_test/my_file_for_test.txt"); 
+    boost::filesystem::create_directories(subdir);
+    std::ofstream ofs("./my_dir_for_test/my_file_for_test.txt");
     ofs.close();
     FileNode dir_node, subdir_node, file_node;
     subdir_node.name = "my_subdir";
@@ -43,26 +43,26 @@ TEST_F(TreeTestCase, TestFour) {
     dir_node.is_dir = true;
     dir_node.children = {subdir_node};
     ASSERT_EQ(GetTree("my_dir_for_test", true), dir_node);
-    std::remove("./my_dir_for_test/my_file_for_test.txt"); 
+    std::remove("./my_dir_for_test/my_file_for_test.txt");
 }
 
 TEST_F(TreeTestCase, TestFive) {
     boost::filesystem::path dir("./my_dir_for_test");
-    boost::filesystem::create_directories(dir); 
+    boost::filesystem::create_directories(dir);
     boost::filesystem::path subdir("./my_dir_for_test/my_subdir");
-    boost::filesystem::create_directories(subdir); 
-    std::ofstream ofs("./my_dir_for_test/my_file_for_test.txt"); 
+    boost::filesystem::create_directories(subdir);
+    std::ofstream ofs("./my_dir_for_test/my_subdir/my_file_for_test.txt");
     ofs.close();
     FileNode dir_node, subdir_node, file_node;
-    subdir_node.name = "my_subdir";
-    subdir_node.is_dir = true;
-    subdir_node.children.clear();
     file_node.name = "my_file_for_test.txt";
     file_node.is_dir = false;
     file_node.children.clear();
+    subdir_node.name = "my_subdir";
+    subdir_node.is_dir = true;
+    subdir_node.children = {file_node};
     dir_node.name = "my_dir_for_test";
     dir_node.is_dir = true;
-    dir_node.children = {file_node, subdir_node};
+    dir_node.children = {subdir_node};
     ASSERT_EQ(GetTree("my_dir_for_test", false), dir_node);
-    std::remove("./my_dir_for_test/my_file_for_test.txt"); 
+    std::remove("./my_dir_for_test/my_file_for_test.txt");
 }
