11818c53e0133938111a8356714cf19e5e031a08
Konstantin <kovalev.ka@phystech.edu>
2020-05-09 13:20:09 +0300

    Сделал третью часть

--- task5/tests/03-weather/WeatherTestCase.cpp
+++ task5/tests/03-weather/WeatherTestCase.cpp
@@ -5,3 +5,54 @@
 #include "WeatherTestCase.h"
 #include "WeatherMock.h"
 
+
+using ::testing::AtLeast;
+using ::testing::Return;
+
+
+cpr::Response MyResponse(int code, const std::string& text) {
+    cpr::Response response = cpr::Response();
+    response.status_code = code;
+    response.text = text;
+    return response;
+}
+
+
+TEST_F(WeatherTestCase, testOne) {
+    WeatherMock mock;
+    ASSERT_NO_THROW(mock.SetApiKey(API));
+}
+
+TEST_F(WeatherTestCase, testTwo) {
+    WeatherMock mock;
+    EXPECT_CALL(mock, Get(moscow)).Times(AtLeast(1));
+    ASSERT_THROW(mock.GetResponseForCity(moscow), std::invalid_argument);
+}
+
+TEST_F(WeatherTestCase, testThree) {
+    WeatherMock mock;
+    EXPECT_CALL(mock, Get(moscow)).Times(AtLeast(1)).WillRepeatedly(Return(MyResponse(good_status_code, default_response_Moscow)));
+    EXPECT_EQ(mock.GetTemperature(moscow), expected_temp);
+}
+
+TEST_F(WeatherTestCase, testFour) {
+    WeatherMock mock;
+    EXPECT_CALL(mock, Get(moscow)).WillRepeatedly(Return(MyResponse(good_status_code, default_response_Moscow)));
+    EXPECT_CALL(mock, Get(dolgoprudny)).WillRepeatedly(Return(MyResponse(good_status_code, default_response_Dolgoprudny)));
+    EXPECT_EQ(mock.GetDifferenceString("Moscow", "Dolgoprudny"), "Weather in Moscow is warmer than in Dolgoprudny by 1260 degrees");
+    EXPECT_EQ(mock.GetDifferenceString("Dolgoprudny", "Moscow"), "Weather in Dolgoprudny is colder than in Moscow by 1260 degrees");
+}
+
+TEST_F(WeatherTestCase, testFive) {
+    WeatherMock mock;
+    EXPECT_CALL(mock, Get(moscow)).WillOnce(Return(MyResponse(good_status_code, default_response_Moscow)));
+    EXPECT_CALL(mock, Get(dolgoprudny)).WillOnce(Return(MyResponse(good_status_code, default_response_Dolgoprudny)));
+    EXPECT_EQ(mock.FindDiffBetweenTwoCities(moscow, dolgoprudny), expected_diff);
+}
+
+TEST_F(WeatherTestCase, TestSix) {
+    json expected = json::parse(responsec_city_JSON);
+    WeatherMock mock;
+    EXPECT_CALL(mock, Get(moscow)).Times(AtLeast(1)).WillOnce(Return(MyResponse(good_status_code, default_response_Moscow)));
+    EXPECT_EQ(mock.GetResponseForCity(moscow), expected);
+}
--- task5/tests/03-weather/WeatherTestCase.h
+++ task5/tests/03-weather/WeatherTestCase.h
@@ -2,9 +2,27 @@
 #pragma once
 
 #include <gtest/gtest.h>
+#include <string>
 
 class WeatherTestCase : public ::testing::Test {
+protected:
+    void SetUp() override {
+        moscow = "Moscow";
+        dolgoprudny = "Dolgoprudny";
+        responsec_city_JSON = "{\"list\":[{\"main\":{\"temp\":1488,\"name\":\"Moscow\",\"pressure\":7777}}]}";
+        good_status_code = 200;
+        expected_diff = 1260;
+        expected_temp = 1488;
+        API = "556689";
+    }
+
+    std::string moscow;
+    std::string dolgoprudny;
+    std::string responsec_city_JSON;
+    std::string default_response_Moscow = "{\"list\":[{\"main\":{\"temp\":1488,\"name\":\"Moscow\",\"pressure\":7777}}]}";
+    std::string default_response_Dolgoprudny = "{\"list\":[{\"main\":{\"temp\":228,\"name\":\"Dolgoprudny\",\"pressure\":7777}}]}";
+    std::string API;
+    int good_status_code;
+    int expected_diff;
+    float expected_temp;
 };
-
-
-
